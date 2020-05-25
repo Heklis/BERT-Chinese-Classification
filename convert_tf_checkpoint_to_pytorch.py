0a7786bed4be8431b00596d969b9de4798171412
Heklis <haoshoudi@qq.com>
2020-05-25 22:22:06 +0800

    Update convert_tf_checkpoint_to_pytorch.py

--- convert_tf_checkpoint_to_pytorch.py
+++ convert_tf_checkpoint_to_pytorch.py
@@ -1,24 +1,3 @@
-# coding=utf-8
-# Copyright 2018 The HuggingFace Inc. team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Convert BERT checkpoint."""
-
-# from __future__ import absolute_import
-# from __future__ import division
-# from __future__ import print_function
-
-import argparse
 import torch
 
 from transformers import BertConfig, BertForPreTraining, load_tf_weights_in_bert
@@ -41,25 +20,13 @@ def convert_tf_checkpoint_to_pytorch(tf_checkpoint_path, bert_config_file, pytor
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    ## Required parameters
-    parser.add_argument("--tf_checkpoint_path",
-                        default = None,
-                        type = str,
-                        required = True,
-                        help = "Path to the TensorFlow checkpoint path.")
-    parser.add_argument("--bert_config_file",
-                        default = None,
-                        type = str,
-                        required = True,
-                        help = "The config json file corresponding to the pre-trained BERT model. \n"
-                            "This specifies the model architecture.")
-    parser.add_argument("--pytorch_dump_path",
-                        default = None,
-                        type = str,
-                        required = True,
-                        help = "Path to the output PyTorch model.")
-    args = parser.parse_args()
-    convert_tf_checkpoint_to_pytorch(args.tf_checkpoint_path,
-                                     args.bert_config_file,
-                                     args.pytorch_dump_path)
+    # Path to the TensorFlow checkpoint path.
+    tf_checkpoint_path = "D:/Heklis/bin/chinese_L-12_H-768_A-12/bert_model.ckpt"
+    # The config json file corresponding to the pre-trained BERT model. 
+    # This specifies the model architecture.
+    bert_config_file = "D:/Heklis/bin/chinese_L-12_H-768_A-12/bert_config.json"
+    # Path to the output PyTorch model.
+    pytorch_dump_path = "D:/Heklis/bin/chinese_L-12_H-768_A-12/pytorch.bin"
+    convert_tf_checkpoint_to_pytorch(tf_checkpoint_path,
+                                     bert_config_file,
+                                     pytorch_dump_path)
